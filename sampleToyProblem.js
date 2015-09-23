20f3dc6a1d3e1053ce20472665e481dd10b444ae
Cheyenne Kellis <cheyenne.kellis@gmail.com>
2015-09-23 11:36:25 -0700

    Solves toy problem function and includes code for separate charCount() function

--- sampleToyProblem.js
+++ sampleToyProblem.js
@@ -10,7 +10,7 @@ Explanation total([1,2,3,4,5]) => 48
           ==>5+7=> 12 / \           20+28 => 48
 3+4=7--\ /            == 12+16=>28/
 4+5=9--/ 7+9 =>     16  /
- 
+
 if total([1,2,3]) => 8 then
 
 first+second => 3 \
@@ -18,8 +18,34 @@ first+second => 3 \
 second+third => 5 /
 
 */
+var arr = [1,2,3,4,5];
 
 var total = function (array) {
-  //write total here
+  var sum = [];
+  if(array.length===1){
+    return array[0];
+  }
+  for(var i = 0; i< array.length - 1; i++){
+    sum[i]=(array[i]+array[i+1]);
+  }
+  return total(sum);
 };
+
+console.log(total(arr));
+
+/*---------------
+var charCount = function(character, string) {
+  var count = 0, length = string.length;
+  if(length === 0){
+    return count;
+  }
+  if(string[0] === character){
+    count ++;
+  }
+  string = string.slice(1);
+  return count + charCount(character, string);
+};
+
+console.log(charCount("o", "noooooo"));
+*/
 
